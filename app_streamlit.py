9bf2fc238d24d8e944ef56558173dade9607342c
Ado912 <sabdowinarah24@gmail.com>
2025-09-08 11:08:57 +0700

    Add files via upload

---
+++ app_streamlit.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+
+
+model = joblib.load("model_klasifikasi_tomat.joblib")
+scaler = joblib.load("scaler_klasifikasi_tomat.joblib")
+
+st.title("Klasifikasi Tomat")
+st.markdown("aplikasi machine learning untuk klasifikasi tomat apakah termasuk kategori **Ekspor,Lokal Premium atau industri** ")
+
+berat = st.slider("Berat Tomat",50, 200, 80)
+kekenyalan = st.slider("Tingkat Kekenyalan",2.0, 10.0, 4.2)
+kadar_gula = st.slider("Kadar Gula", 1.0, 10.0, 5.3)
+tebal_kulit = st.slider("Tebal Kulit", 0.1, 1.0, 0.6)
+
+
+if st.button("Prediksi",type="primary"):
+	data_baru = pd.DataFrame([[berat,kekenyalan,kadar_gula,tebal_kulit]],columns=["berat","kekenyalan","kadar_gula","tebal_kulit"])
+	data_baru_scaled = scaler.transform(data_baru)
+	prediksi = model.predict(data_baru_scaled)[0]
+	presentase= max(model.predict_proba(data_baru_scaled)[0])
+	st.success(f"Prediksi **{prediksi}** keyakinan **{presentase*100:.2f}%**")
+	st.snow()
+st.divider()
+st.caption("dibuat oleh **sabdo**")
